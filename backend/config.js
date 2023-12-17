07f066e2974df92564f5eb925339939755e8ddea
keerthireddy <nkeerthireddy44@gmail.com>
2023-12-17 17:46:29 +0530

    first time

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+// export const mongoDBURL =
+//   'mongodb+srv://root:root@books-store-mern.c7ba8z1.mongodb.net/books-collection?retryWrites=true&w=majority';
+  export const mongoDBURL ='mongodb+srv://ecommerceApp:QqkFvnlqW5qXMeQR@cluster0.ybw7hzj.mongodb.net/bookstore';
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
